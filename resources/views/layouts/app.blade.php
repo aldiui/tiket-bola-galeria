749cc2451d9d2d60699415633f5f26d16d70e873
aldiui <aldijaya280902@gmail.com>
2024-01-29 19:22:18 +0700

    integrasi pwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 	<link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}" />
 	<link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
     @stack('style')
+	@laravelPWA
 </head>
 
 <body>
