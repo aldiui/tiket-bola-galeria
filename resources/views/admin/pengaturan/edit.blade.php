1bf7c545bdfa261ea0fb85f02079f8b16dff625b
aldiui <aldijaya280902@gmail.com>
2024-02-13 02:18:03 +0700

    add setup view controller in admin

--- resources/views/admin/pengaturan/edit.blade.php
+++ resources/views/admin/pengaturan/edit.blade.php
@@ -1,4 +1,5 @@
-<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+    data-bs-backdrop="static" data-bs-keyboard="false">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
@@ -25,26 +26,40 @@
                         <small class="invalid-feedback" id="errorpassword"></small>
                     </div>
                     <div class="form-group mb-3">
-                        <label for="password_confirmation" class="form-label">Konfirmasi Password <span class="text-danger">*</span></label>
-                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                        <label for="password_confirmation" class="form-label">Konfirmasi Password <span
+                                class="text-danger">*</span></label>
+                        <input type="password" class="form-control" id="password_confirmation"
+                            name="password_confirmation">
                         <small class="invalid-feedback" id="errorpassword_confirmation"></small>
                     </div>
                     <div class="form-group">
                         <label class="form-label">Hak Akses</label>
                         <div class="d-flex gap-1 flex-wrap">
-                            <input type="checkbox" class="btn-check" name="tambah_pengunjung_masuk" id="tambah_pengunjung_masuk_edit" autocomplete="off" value="1">
-                            <label class="btn btn-outline-primary" for="tambah_pengunjung_masuk_edit">Tambah Pengunjung Masuk</label>
-                            <input type="checkbox" class="btn-check" name="tambah_pengunjung_keluar" id="tambah_pengunjung_keluar_edit" autocomplete="off" value="1">
-                            <label class="btn btn-outline-primary" for="tambah_pengunjung_keluar_edit">Tambah Pengunjung Keluar</label>
-                            <input type="checkbox" class="btn-check" name="riwayat_pengunjung_masuk" id="riwayat_pengunjung_masuk_edit" autocomplete="off" value="1">
-                            <label class="btn btn-outline-primary" for="riwayat_pengunjung_masuk_edit">Riwayat Pengunjung Masuk</label>
-                            <input type="checkbox" class="btn-check" name="riwayat_pengunjung_keluar" id="riwayat_pengunjung_keluar_edit" autocomplete="off" value="1">
-                            <label class="btn btn-outline-primary" for="riwayat_pengunjung_keluar_edit">Riwayat Pengunjung Keluar</label>
-                            <input type="checkbox" class="btn-check" name="laporan_keuangan" id="laporan_keuangan_edit" autocomplete="off" value="1">
-                            <label class="btn btn-outline-primary" for="laporan_keuangan_edit">Laporan Keuangan</label>
-                            <input type="checkbox" class="btn-check" name="user_management" id="user_management_edit" autocomplete="off" value="1">
+                            <input type="checkbox" class="btn-check" name="tambah_pengunjung_masuk"
+                                id="tambah_pengunjung_masuk_edit" autocomplete="off" value="1">
+                            <label class="btn btn-outline-primary" for="tambah_pengunjung_masuk_edit">Tambah Pengunjung
+                                Masuk</label>
+                            <input type="checkbox" class="btn-check" name="tambah_pengunjung_keluar"
+                                id="tambah_pengunjung_keluar_edit" autocomplete="off" value="1">
+                            <label class="btn btn-outline-primary" for="tambah_pengunjung_keluar_edit">Tambah Pengunjung
+                                Keluar</label>
+                            <input type="checkbox" class="btn-check" name="riwayat_pengunjung_masuk"
+                                id="riwayat_pengunjung_masuk_edit" autocomplete="off" value="1">
+                            <label class="btn btn-outline-primary" for="riwayat_pengunjung_masuk_edit">Riwayat
+                                Pengunjung Masuk</label>
+                            <input type="checkbox" class="btn-check" name="riwayat_pengunjung_keluar"
+                                id="riwayat_pengunjung_keluar_edit" autocomplete="off" value="1">
+                            <label class="btn btn-outline-primary" for="riwayat_pengunjung_keluar_edit">Riwayat
+                                Pengunjung Keluar</label>
+                            <input type="checkbox" class="btn-check" name="laporan_keuangan" id="laporan_keuangan_edit"
+                                autocomplete="off" value="1">
+                            <label class="btn btn-outline-primary" for="laporan_keuangan_edit">Laporan
+                                Keuangan</label>
+                            <input type="checkbox" class="btn-check" name="user_management"
+                                id="user_management_edit" autocomplete="off" value="1">
                             <label class="btn btn-outline-primary" for="user_management_edit">User Management</label>
-                            <input type="checkbox" class="btn-check" name="ubah_tarif" id="ubah_tarif_edit" autocomplete="off" value="1">
+                            <input type="checkbox" class="btn-check" name="ubah_tarif" id="ubah_tarif_edit"
+                                autocomplete="off" value="1">
                             <label class="btn btn-outline-primary" for="ubah_tarif_edit">Ubah Tarif</label>
                         </div>
                     </div>
