24d45647980c8ed1ba338ff2420f05be1e1d7d7e
aldiui <aldijaya280902@gmail.com>
2024-02-18 20:58:31 +0700

    updated no push ftp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,36 @@
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-	
-	<link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}" />
-	<link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
+
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
     @stack('style')
-	@laravelPWA
+    @laravelPWA
 </head>
 
 <body>
-	<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-		data-sidebar-position="fixed" data-header-position="fixed">
-		@include('components.sidebar')
-		<div class="body-wrapper">
-			@include('components.header')
-			<div class="container-fluid">
-				@yield('main')
-				@include('components.footer')
-			</div>
-		</div>
-	</div>
-	<script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
-	<script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-	<script src="{{ asset('js/sidebarmenu.js') }}"></script>
-	<script src="{{ asset('js/app.min.js') }}"></script>
-	<script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
-	<script src="{{ asset('js/setup.js') }}"></script>
-	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        @include('components.sidebar')
+        <div class="body-wrapper">
+            @include('components.header')
+            <div class="container-fluid">
+                @yield('main')
+                @include('components.footer')
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('js/app.min.js') }}"></script>
+    <script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
+    <script src="{{ asset('js/setup.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     @stack('scripts')
 </body>
